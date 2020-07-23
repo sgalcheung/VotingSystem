6d0c7e90a02f8daa848ffc714baddf4b2c4e8a97
sgalcheung <sgalwork@outlook.com>
2020-07-23 16:36:39 +0800

    send email to confirm account

---
+++ Code/VotingSystem/VotingSystem.Web/Services/AuthMessageSenderOptions.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VotingSystem.Web.Services
+{
+    public class AuthMessageSenderOptions
+    {
+        public string SendEmailUser { get; set; }
+        public string SendEmailKey { get; set; }
+    }
+}
--- Code/VotingSystem/VotingSystem.Web/Startup.cs
+++ Code/VotingSystem/VotingSystem.Web/Startup.cs
@@ -7,11 +7,13 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.UI;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Identity.UI.Services;
 using Microsoft.EntityFrameworkCore;
 using VotingSystem.Web.Data;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using VotingSystem.Web.Services;
 
 namespace VotingSystem.Web
 {
@@ -32,6 +34,10 @@ namespace VotingSystem.Web
                     Configuration.GetConnectionString("DefaultConnection")));
             services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
                 .AddEntityFrameworkStores<ApplicationDbContext>();
+
+            services.AddTransient<IEmailSender, EmailSender>();
+            services.Configure<AuthMessageSenderOptions>(Configuration);
+
             services.AddControllersWithViews();
             services.AddRazorPages();
         }
