6c697add42f43a29fdcac26e290e9c3ab654a79c
sgalcheung <sgalwork@outlook.com>
2020-07-31 17:52:38 +0800

    dynamic addition and deletion of items can be realized by editing

--- Code/VotingSystem/VotingSystem.Web/Models/CreateVoteCommand.cs
+++ Code/VotingSystem/VotingSystem.Web/Models/CreateVoteCommand.cs
@@ -8,6 +8,16 @@ namespace VotingSystem.Web.Models
 {
     public class CreateVoteCommand : EditVoteBase
     {
+        /// <summary>
+        /// 为了创建的时候初始化三个
+        /// </summary>
+        public CreateVoteCommand()
+        {
+            VoteItems.Add(new CreateVoteItemCommand());
+            VoteItems.Add(new CreateVoteItemCommand());
+            VoteItems.Add(new CreateVoteItemCommand());
+        }
+
         public IList<CreateVoteItemCommand> VoteItems { get; set; } = new List<CreateVoteItemCommand>();
 
         public Vote ToVote(string createById)
