ba61656ba1d397977e4386afda1d838ba726d78a
sgalcheung <sgalwork@outlook.com>
2020-09-23 13:52:30 +0800

    modify program exposed port

--- src/VotingSystem/Program.cs
+++ src/VotingSystem/Program.cs
@@ -14,7 +14,9 @@ namespace VotingSystem
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
-                    webBuilder.UseStartup<Startup>();
+                    webBuilder
+                        .UseUrls("http://*:5000")
+                        .UseStartup<Startup>();
                 });
     }
 }
